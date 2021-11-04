0c68b413bd0839078af38a01230c967402966c5e
Manish Goud Bandharapu <manishgoud101@gmail.com>
2021-11-03 19:46:57 -0500

    created selector

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var carRouter = require('./routes/car');
 var addmodsRouter = require('./routes/addmods');
-
+var selectorRouter = require('./routes/selector');
 
 
 var app = express();
@@ -27,6 +27,7 @@ app.use('/car', carRouter);
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/addmods', addmodsRouter);
+app.use('/selector', selectorRouter);
 
 
 // catch 404 and forward to error handler
